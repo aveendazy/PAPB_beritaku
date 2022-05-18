dc4868a4218382ca7d78365dc696ea8dfc3ba9b9
Salsabila Riskiani Gusti Putri <salsaputri407@gmail.com>
2022-05-18 09:30:30 +0700

    toolbar

--- app/src/main/java/com/example/beritaku/MainActivity.java
+++ app/src/main/java/com/example/beritaku/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.beritaku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView righticon = findViewById(R.id.right_icon);
+        TextView title = findViewById(R.id.toolbar_title);
+
+        righticon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You clicked in right icon", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        title.setText("beritaku");
     }
 }
